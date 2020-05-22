63848460a5c2c3207a795784f9670ba67cbce75d
RiteshEdupuganti <65661577+RiteshEdupuganti@users.noreply.github.com>
2020-05-22 17:54:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,10 +2,15 @@ var fixedRect,movingRect
 
 function setup() {
   createCanvas(1200,800);
+  
  fixedRect = createSprite(200, 200, 50, 80);
  fixedRect.shapeColor = "green";
+
  movingRect = createSprite(400,200,80,30);
  movingRect.shapeColor = "green";
+ 
+ movingRect.velocityY = -5;
+ fixedRect.velocityY = 5;
 }
 
 function draw() {
@@ -31,6 +36,21 @@ function draw() {
       fixedRect.shapeColor = "green";
       movingRect.shapeColor = "green";
     }
+
+    if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2 
+      && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2) 
+      { 
+        movingRect.velocityX = movingRect.velocityX * (-1);
+        fixedRect.velocityX = fixedRect.velocityX * (-1); 
+      }
+    
+   if (movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2 
+      && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2)
+      { 
+        movingRect.velocityY = movingRect.velocityY * (-1);
+        fixedRect.velocityY = fixedRect.velocityY * (-1); 
+      }
+
 
 drawSprites();
 }
